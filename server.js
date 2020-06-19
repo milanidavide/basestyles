b3d04c4d5450cddb227e1d14d48bbbe77ef0b3ad
Davide Milani <milanidavide@milanidavide.com>
2020-06-19 13:53:50 +0200

    Modified express server path

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('dist'));
+app.use(express.static('.'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
